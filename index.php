24ac898e0121440291c7820decc683ee3eb437e9
erika nataly <erika_nataly.123@hotmail.com>
2016-03-08 13:01:23 -0500

    portada

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<div class="contenedor">
+
+	<nav>
+		<ul class="menu-lista">	 
+			<li class="menu-item"><a href="" class="menu-link">INCIO</a></li>
+			<li class="menu-item"><a href="" class="menu-link">GALERIA</a></li>
+			<li class="menu-item"><a href="" class="menu-link">BLOG</a></li>
+			<li class="menu-item"><a href="" class="menu-link">ANUNCIOS</a></li>
+			<li class="menu-item"><a href="" class="menu-link">CONTACTENOS</a></li>
+		</ul>
+	</nav>
+</div class src="slider">
+<img src="porta.jpg" height="650">
+<img src="foto 1.jpg" height="250">
+<img src="foto 2.jpg" height="250">
+<img src="foto 3.jpg" height="250">
+<img src="foto 4.jpg" height="250">
+</body>
+</html>
